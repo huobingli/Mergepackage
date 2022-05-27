b4f8d5f90387d860f61dbdbeee7c576c1e1aa729
huobingli <huobingli@outlook.com>
2022-05-27 18:36:14 +0800

    clean

--- server/main.go
+++ server/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -58,16 +55,10 @@ func main() {
 	})
 
 	// 增加接口
-	router.POST("/upload_file", HandleUploadFile)
-	router.POST("/upload_muti_file", HandleUploadMutiFile)
-	router.GET("/download", HandleDownloadFile)
-	router.GET("/getUrl", Get)
-	router.GET("/file", HandleShowFile)
 	router.GET("/CallMergePackage", CallMergePackage)
 	router.GET("/CallZipPackage", CallZipPackage)
 
-	// 文件服务器
-	// path := "D:\\gitProject\\Mergepackage\\server\\package"
+	// 静态文件服务
 	fmt.Println(cf.Zip_dir)
 	fmt.Println(cf.Xd_dir)
 	fmt.Println(cf.Jrzd_dir)
@@ -110,110 +101,4 @@ func CallZipPackage(c *gin.Context) {
 	}
 
 	fmt.Println("%s", out.String())
-}
-
-//错误
-func handleError(err error) {
-	fmt.Println("Error:", err)
-	os.Exit(-1)
-}
-
-// 发送GET请求
-// url：         请求地址
-// response：    请求返回的内容
-func Get(c *gin.Context) {
-	url := c.Param("url")
-	// 超时时间：5秒
-	client := &http.Client{Timeout: 5 * time.Second}
-	resp, err := client.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	var buffer [512]byte
-	result := bytes.NewBuffer(nil)
-	for {
-		n, err := resp.Body.Read(buffer[0:])
-		result.Write(buffer[0:n])
-		if err != nil && err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
-	}
-	c.JSON(200, gin.H{"status": 1, "result": result.String(), "message": "Success"})
-}
-
-// HandleUploadFile 上传单个文件
-func HandleUploadFile(c *gin.Context) {
-	file, header, err := c.Request.FormFile("file")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "文件上传失败"})
-		return
-	}
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "文件读取失败"})
-		return
-	}
-	fmt.Println(header.Filename)
-	fmt.Println(string(content))
-	c.JSON(http.StatusOK, gin.H{"msg": "上传成功"})
-}
-
-// HandleUploadMutiFile 上传多个文件
-func HandleUploadMutiFile(c *gin.Context) {
-	// 限制上传文件大小
-	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 4<<20)
-	// 限制放入内存的文件大小
-	err := c.Request.ParseMultipartForm(4 << 20)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "文件读取失败"})
-		return
-	}
-	formdata := c.Request.MultipartForm
-	files := formdata.File["file"]
-	for _, v := range files {
-		file, err := v.Open()
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"msg": "文件读取失败"})
-			return
-		}
-		defer file.Close()
-
-		content, err := ioutil.ReadAll(file)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"msg": "文件读取失败"})
-			return
-		}
-		fmt.Println(v.Filename)
-		fmt.Println(string(content))
-	}
-	c.JSON(http.StatusOK, gin.H{"msg": "上传成功"})
-}
-
-// HandleDownloadFile 下载文件
-func HandleDownloadFile(c *gin.Context) {
-	//content := c.Query("content")
-	//content = "hello world, 我是一个文件，" + content
-
-	filename := c.Query("content")
-	path := "D:\\ci\\autoBuild\\cd-tool\\output\\build\\"
-	filepath := path + filename
-	c.Writer.WriteHeader(http.StatusOK)
-	// 返回文件名
-	c.Header("Content-Disposition", "attachment; filename="+filename)
-	c.Header("Content-Type", "application/text/plain")
-	c.File(filepath)
-}
-
-func HandleShowFile(c *gin.Context) {
-	path := "D:\\ci\\autoBuild\\cd-tool\\output\\build\\"
-	fileName := path + c.Query("name")
-	fmt.Println(fileName)
-	c.File(fileName)
-
-	//http.FileServer(http.Dir(BaseUploadPath))
-	//http.FileServer()
-
 }
